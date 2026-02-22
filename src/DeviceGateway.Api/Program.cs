75d52f0d9be8890d93aeb0d8f62afabc473e4138
Gustavo Nogueira <gutoreis.nogueir@gmail.com>
2026-02-22 00:39:48 +0000

    feat: integrate Serilog

--- src/DeviceGateway.Api/Program.cs
+++ src/DeviceGateway.Api/Program.cs
@@ -2,29 +2,63 @@ using DeviceGateway.Api;
 using DeviceGateway.Application;
 using DeviceGateway.Domain;
 using DeviceGateway.Infrastructure;
+using Serilog;
+using Serilog.Events;
+using Serilog.Formatting.Json;
 
-var builder = WebApplication.CreateBuilder(args);
+// Two-stage Logger Initialization. See more on https://github.com/serilog/serilog-aspnetcore#two-stage-initialization
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services
-    .AddOpenApi()
-    .AddApi(builder.Configuration)
-    .AddApplication(builder.Configuration)
-    .AddDomain(builder.Configuration)
-    .AddInfrastructure(builder.Configuration);
-
-
-var app = builder.Build();
-
-app.MapEndpoints();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+try
 {
-    app.MapOpenApi();
+    Log.Information("Starting web host");
+    var builder = WebApplication.CreateBuilder(args);
+
+    // Setup Logging
+    builder.Host.UseSerilog((context, services, configuration) =>
+    {
+        configuration
+            .ReadFrom.Configuration(context.Configuration)
+            .ReadFrom.Services(services)
+            .Enrich.FromLogContext()
+            .WriteTo.Console(new JsonFormatter());
+    });
+
+    Log.Information("Configuring services");
+    // Add Services
+    builder.Services
+        .AddOpenApi()
+        .AddApi(builder.Configuration)
+        .AddApplication(builder.Configuration)
+        .AddDomain(builder.Configuration)
+        .AddInfrastructure(builder.Configuration);
+
+    var app = builder.Build();
+
+    Log.Information("Configuring middleware");
+    // Add Endpoints
+    app.MapEndpoints();
+
+    // Configure Dev only env
+    if (app.Environment.IsDevelopment())
+    {
+        app.MapOpenApi();
+    }
+
+    app.UseHttpsRedirection();
+
+    Log.Information("Starting application");
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-
-app.Run();
+catch (Exception e)
+{
+    Log.Fatal(e, "Host terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
