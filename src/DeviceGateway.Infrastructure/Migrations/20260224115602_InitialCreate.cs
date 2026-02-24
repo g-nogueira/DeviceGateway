10363d5b9920234f1cdc19fbb920e237fc7fe981
Gustavo Nogueira <gutoreis.nogueir@gmail.com>
2026-02-24 12:02:45 +0000

    feat: add initial migration and device entity with configuration

---
+++ src/DeviceGateway.Infrastructure/Migrations/20260224115602_InitialCreate.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DeviceGateway.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Devices",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    BrandId = table.Column<Guid>(type: "uuid", nullable: false),
+                    State = table.Column<int>(type: "integer", nullable: false),
+                    CreatedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false, defaultValueSql: "CURRENT_TIMESTAMP"),
+                    DeletedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Devices", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Devices_BrandId",
+                table: "Devices",
+                column: "BrandId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Devices_CreatedAt",
+                table: "Devices",
+                column: "CreatedAt");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Devices_DeletedAt",
+                table: "Devices",
+                column: "DeletedAt");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Devices_State",
+                table: "Devices",
+                column: "State");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Devices");
+        }
+    }
+}
